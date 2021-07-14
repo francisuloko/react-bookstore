e94d8ebba83080770b7071d61b01baf2251b9886
Francis Uloko <francisuloko@yahoo.com>
2021-07-14 22:25:27 +0100

    Add SPA navigation functionality

--- javascript-exercises/my-awesome-books/assets/javascript/library.js
+++ javascript-exercises/my-awesome-books/assets/javascript/library.js
@@ -1,16 +1,36 @@
 export default class Library {
     constructor(){
       this.books = [];
+      this.bookList = document.getElementById("book-list");
       this.localStorageBooks = JSON.parse(localStorage.getItem('books'));
+      this.notice = document.getElementById('notice')
+
       if(this.localStorageBooks == null){
         localStorage.setItem('books', JSON.stringify([]));
       } else {
-        this.display();
+        this.displayBooks();
+        this.displaySection();
       }
     }
 
-    display(){
-      document.getElementById("bookList").innerHTML = '';
+    save(){
+      localStorage.setItem('books', JSON.stringify(this.books));
+    }
+
+    createBook(book, id){
+      let bookItem = `<article class='book'>
+        <div class="info">
+          <span class='title'>"${book.title}" by </span>
+          <span class='author'>${book.author}</span>
+        </div>
+        <button type='button' class='remove-button' id=${id}>Remove</button>
+      </article>`;
+    
+      this.bookList.innerHTML += bookItem;
+    }
+
+    displayBooks(){
+      document.getElementById("book-list").innerHTML = '';
       const books = this.localStorageBooks;
       for(let i = 0; i < books.length; i++){
         this.createBook(books[i], i);
@@ -25,53 +45,47 @@ export default class Library {
       };
     }
 
-    createBook(book, id){
-      let bookList = document.getElementById("bookList");
-      let bookItem = `<article class='book'>
-        <div class="info">
-          <span class='title'>"${book.title}" by </span>
-          <span class='author'>${book.author}</span>
-        </div>
-        <button type='button' class='remove-button' id=${id}>Remove</button>
-      </article>`;
-    
-      bookList.innerHTML += bookItem;
-    }
-
     add(book){
-      this.books = this.localStorageBooks;
-      this.books.push(book);
-      localStorage.setItem('books', JSON.stringify(this.books));
-      this.display();
+      if(book.title !== '' && book.author !== ''){
+        this.books = this.localStorageBooks;
+        this.books.push(book);
+        this.save()
+        this.displayNotice(['success', `${book.title} added successfully!`])
+        this.displayBooks();
+      } else {
+        this.displayNotice(['fail', 'Field(s) cannot be empty']);
+      }
     }
 
     remove(id){
       this.books = this.localStorageBooks;
       let book = this.books.splice(id, 1)
-      this.notice(['success', `${book[0].title} removed successfully!`])
-      localStorage.setItem('books', JSON.stringify(this.books));
-      this.display();
+      this.displayNotice(['success', `${book[0].title} removed successfully!`])
+      this.save()
+      this.displayBooks();
     }
 
-    showPage() {
-      let sections = document.getElementsByClassName('spa-links');
-      sections.forEach(section, ()=>{
-        section.classList.toggle("show-page");
-      });
-    }
-
-    notice(message){
-      let notice = document.getElementById('notice')
+    displayNotice(message){
       if(message[0] == 'success'){
-        notice.style.color = 'green';
-        notice.textContent = message[1];
+        this.notice.style = 'color: green';
+        this.notice.textContent = message[1];
       } else {
-        notice.style.color = 'red';
-        notice.textContent = message[1];
+        this.notice.style = 'color: red';
+        this.notice.textContent = message[1];
       }
 
       setTimeout(() => {
-        notice.innerHTML = '';
+        this.notice.innerHTML = '';
       }, 2000);
+    }
+
+    displaySection(link){
+      if(link == 101){
+        document.getElementById('new-book-form').style.display = 'block'
+        document.getElementById('books-section').style.display = 'none'
+      } else {
+        document.getElementById('books-section').style.display = 'block'
+        document.getElementById('new-book-form').style.display = 'none'
+      }
     }
 }
--- javascript-exercises/my-awesome-books/assets/javascript/main.js
+++ javascript-exercises/my-awesome-books/assets/javascript/main.js
@@ -8,54 +8,17 @@ document.getElementById('add-button').addEventListener('click', (event) => {
   event.preventDefault();
   let title = document.getElementById("book-title");
   let author = document.getElementById("author");
-  
-  if(title.value !== '' && author.value !== ''){
-    let book = new Book(title.value, author.value);
-    title.value = '';
-    author.value = '';
-    library.add(book);
-    library.notice(['success', `${book.title} added successfully`]);
-  } else {
-    library.notice(['fail', 'Field(s) cannot be empty']);
-  }
+  let book = new Book(title.value, author.value);
+  title.value = '';
+  author.value = '';
+  library.add(book);
 });
 
-
-// function showSinglePage(){
-//   let singlePageApp = document.getElementById('single-page-app');
-
-//   let sectionOne = `
-//   <section id='display-books'  class="show-page">
-//     <h1>My Awesome Books</h1>
-//     <div id='bookList' class="book-list"></div>
-//   </section>`;
-
-//   let sectionTwo = `
-//   <section id='new-book-form' class="new-book-form">
-//     <h2>Add new book</h2>
-//     <form>
-//       <div>
-//         <input type="text" name="book-title" id="book-title" placeholder="Title">
-//       </div>
-
-//       <div>
-//         <input type="text" name="author" id="author" placeholder="Author">
-//       </div>
-      
-//       <button type="button" value="Add" id="add-button">Add book</button>
-//     </form>
-//   </section>`;
-
-//   let links = document.querySelectorAll('.nav-links');
-//   console.log()
-//   links.forEach(link, (event)=> {
-//     event.preventDefault();
-//     singlePageApp = '';
-
-//     if(link.id === 'link-1'){
-//       singlePageApp += sectionOne;
-//     } else if(link.id === 'link-2'){
-//       singlePageApp += sectionTwo;
-//     }
-//   });
-// }
+const links = document.querySelectorAll('.links')
+// console.log(links);
+links.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    library.displaySection(link.id);
+  });
+});
