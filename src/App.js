55e20be689eb6579930bdedfca0699bcc217e5f6
Francis Uloko <francisuloko@yahoo.com>
2021-08-31 19:43:37 +0100

    Add project files correctly

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Books from './redux/books/books';
+import New from './redux/books/new';
+import Categories from './redux/categories/categories';
+import Nav from './components/nav';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+        <Route path="/">
+          <New />
+          <Books />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
